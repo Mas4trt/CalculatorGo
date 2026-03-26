f8efacb23a22770aeb5da614761a8f7ab952bcca
Yaroslav <fokrivosheev@mail.ru>
2026-03-26 18:42:02 +0500

    Написан метод get и post

--- CalculatorAppBackend/main.go
+++ CalculatorAppBackend/main.go
@@ -1,7 +1,74 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/Knetic/govaluate"
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+type Calculation struct {
+	ID         string `json:"id"`
+	Expression string `json:"expression"`
+	Result     string `json:"result"`
+}
+
+type CalculationRequest struct {
+	Expression string `json:"expression"`
+}
+
+var calculations = []Calculation{}
+
+func calculaExpression(expression string) (string, error) {
+	expr, err := govaluate.NewEvaluableExpression(expression)
+	if err != nil {
+		return "", err
+	}
+	result, err := expr.Evaluate(nil)
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%v", result), err
+}
+
+func getCalculators(c echo.Context) error {
+	return c.JSON(http.StatusOK, calculations)
+}
+
+func postCalculators(c echo.Context) error {
+	var req CalculationRequest
+	err := c.Bind(&req)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, "invalid request")
+	}
+
+	result, err := calculaExpression(req.Expression)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, "invalid request")
+	}
+
+	calc := Calculation{
+		ID:         uuid.NewString(),
+		Expression: req.Expression,
+		Result:     result,
+	}
+	calculations = append(calculations, calc)
+
+	return c.JSON(http.StatusCreated, calc)
+
+}
 
 func main() {
-	fmt.Printf("Version:")
+	e := echo.New()
+
+	e.Use(middleware.CORS())
+	e.Use(middleware.Logger())
+
+	e.GET("/calculations", getCalculators)
+	e.POST("/calculations", postCalculators)
+
+	e.Start("localhost:8080")
 }
